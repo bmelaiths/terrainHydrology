c9cecc4be433a7217e7301f453a8042057f7974b
wattzhikang <zjwatt@iastate.edu>
2021-04-11 19:01:37 -0500

    CPP program can now receive contour array

--- src/buildRivers.cpp
+++ src/buildRivers.cpp
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <vector>
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 /*
 A list of data structures that will be used:
@@ -11,15 +15,91 @@ A list of data structures that will be used:
 
 int main() {
     //gather inputs
-    char i, j;
-    fread(&i, 1, 1, stdin);
-    fread(&j, 1, 1, stdin);
+    // char i, j;
+    // fread(&i, 1, 1, stdin);
+    // fread(&j, 1, 1, stdin);
+    int64_t numPoints;
+    int64_t xSize, ySize;
+    fread(&numPoints, sizeof(int64_t), 1, stdin);
+    fread(&xSize, sizeof(int64_t), 1, stdin);
+    fread(&ySize, sizeof(int64_t), 1, stdin);
+
+    #define X 0
+    #define Y 1
+    float inPoints[numPoints][2];
+    for (int64_t i = 0; i < numPoints; i++)
+    {
+      // TODO adjust for endianness and size for differnt hardware
+      // Points are recorded as y,x
+      fread(inPoints[i]+Y, sizeof(float), 1, stdin);
+      fread(inPoints[i]+X, sizeof(float), 1, stdin);
+      // printf("Point %ld: (%f, %f)\n", i, inPoints[i][0], inPoints[i][1]);
+    }
+
 
     //perform necessary computations
-    int result = i << j;
+    // int result = i << j;
+    std::vector<cv::Point> contour;
+    for (int64_t i = 0; i < numPoints; i++)
+    {
+      // Points in a contour array are y,x
+      contour.push_back(cv::Point2f(inPoints[i][X],inPoints[i][Y]));
+    }
+
+    cv::Mat raw_dist(cv::Size(xSize, ySize), CV_32F);
+    for (int64_t y = 0; y < ySize; y++)
+    {
+      for (int64_t x = 0; x < xSize; x++)
+      {
+        raw_dist.at<float>(x,y) = (float) cv::pointPolygonTest(
+          contour,
+          cv::Point2f((float) x, (float) y),
+          true
+        );
+      }
+    }
+
+    double minVal, maxVal;
+    cv::Point maxDistPt; //inscribed circle center
+    cv::minMaxLoc(raw_dist, &minVal, &maxVal, NULL, &maxDistPt);
+    minVal = abs(minVal);
+    maxVal = abs(maxVal);
+
+    cv::Mat drawing = cv::Mat::zeros(cv::Size(xSize, ySize), CV_8UC3);
+    for (int64_t y = 0; y < ySize; y++)
+    {
+      for (int64_t x = 0; x < xSize; x++)
+      {
+        if( raw_dist.at<float>(x,y) < 0 )
+        {
+          drawing.at<cv::Vec3b>(x,y)[0] = (uchar)(255 - abs(raw_dist.at<float>(x,y)) * 255 / minVal);
+        }
+        else if( raw_dist.at<float>(x,y) > 0 )
+        {
+          drawing.at<cv::Vec3b>(x,y)[2] = (uchar)(255 - raw_dist.at<float>(x,y) * 255 / maxVal);
+        }
+        else
+        {
+          drawing.at<cv::Vec3b>(x,y)[0] = 255;
+          drawing.at<cv::Vec3b>(x,y)[1] = 255;
+          drawing.at<cv::Vec3b>(x,y)[2] = 255;
+        }
+      }
+    }
+
+    cv::imshow("See if this works", drawing);
+
+    cv::waitKey(2000);
 
     //export outputs
+    int result = 1;
     fwrite(&result, 1, 1, stdout);
+    // printf("Number of points: %ld\n", numPoints);
+    // printf("X Size: %ld\tY Size: %ld\n", xSize, ySize);
+    // for (int64_t i = 0; i < numPoints; i++)
+    // {
+    //   printf("Point %ld: (%d, %d)\n", i, contour[i].x, contour[i].y);
+    // }
 
     //free resources
 
--- src/testCPP.py
+++ src/testCPP.py
@@ -4,6 +4,8 @@ import os.path
 import subprocess
 import sys
 import struct
+import cv2 as cv
+import numpy as np
 
 buildRiversExe = 'buildRivers' # path to the executable
 
@@ -11,14 +13,47 @@ if not os.path.exists(buildRiversExe):
     print('The executable does not exist. Run "make" to build it.')
     exit()
 
-i = int(2).to_bytes(1, sys.byteorder, signed=False)
-j = int(2).to_bytes(1, sys.byteorder, signed=False)
+# i = int(2).to_bytes(1, sys.byteorder, signed=False)
+# j = int(2).to_bytes(1, sys.byteorder, signed=False)
 
-input = i + j
+# input = i + j
+
+# Create an image
+r = 100
+src = np.zeros((4*r, 4*r), dtype=np.uint8)
+
+# Create a sequence of points to make a contour
+vert = [None]*6
+vert[0] = (3*r//2, int(1.34*r))
+vert[1] = (1*r, 2*r)
+vert[2] = (3*r//2, int(2.866*r))
+vert[3] = (5*r//2, int(2.866*r))
+vert[4] = (3*r, 2*r)
+vert[5] = (5*r//2, int(1.34*r))
+
+# Draw it in src
+for i in range(6):
+    cv.line(src, vert[i],  vert[(i+1)%6], ( 255 ), 3)
+
+# Get the contours
+contours, _ = cv.findContours(src, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+binary = struct.pack('l', len(contours[0]))
+
+binary = binary + struct.pack('l', 4*r) + struct.pack('l', 4*r)
+
+# points in a contour array are y,x
+for point in contours[0]:
+    point = point[0]
+    binary = binary + struct.pack('f', float(point[0])) + struct.pack('f', float(point[1]))
+
+# file = open('binaryFile', 'w+b')
+# file.write(binary)
+# file.close();
 
 proc = subprocess.run(
     ['./' + buildRiversExe],
-    input=input,
+    input=binary,
     capture_output=True
 )
 
